96e66a83944b751091e9a5930d0f2b59c904b153
javohirh <khasanovjavokhir2004@gmail.com>
2024-09-24 17:25:32 +0500

    fixed

--- src/layouts/LoginLayout.jsx
+++ src/layouts/LoginLayout.jsx
@@ -5,7 +5,7 @@ import back from "../assets/images/back-to.png";
 
 function LoginLayout() {
   return (
-    <div className=" min-h-screen ">
+    <div className=" min-h-screen relative ">
       <Header />
       <NavLink className={"absolute top-[128px] left-[120px]"} to={"/"}>
         {" "}
@@ -14,6 +14,10 @@ function LoginLayout() {
       <div className="max-w-[380px] mx-auto">
         <Outlet />
       </div>
+      <p className="max-w-[360px] text-center text-gray-700 absolute left-2/4 translate-x-[-50%] bottom-6 ">
+        Нажимая на кнопку "Регистрация" Вы соглашаетесь с{" "}
+        <span className="text-main-red"> “Универсальным договором”</span>
+      </p>
     </div>
   );
 }
